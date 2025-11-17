0af08c5a64bd439f3ba82463a08e3bacf1e5db36
robogg133 <luizfernandojoaquimdasilva@proton.me>
2025-11-17 13:41:02 -0300

    reading slices

--- goon/utils.go
+++ goon/utils.go
@@ -20,6 +20,21 @@ func recognizeType(s string) (reflect.Value, error) {
 	case strings.ContainsAny(s, "1234567890") && !strings.Contains(s, "\"") && strings.ContainsAny(s, "."):
 		f, err := strconv.ParseFloat(s, 64)
 		return reflect.ValueOf(f), err
+	case strings.ContainsAny(s, ",") && !strings.HasPrefix(s, "\""):
+		var values []reflect.Value
+
+		split := strings.Split(s, ",")
+
+		for _, v := range split {
+			str := strings.TrimSpace(v)
+			str, _ = strings.CutPrefix(s, "\"")
+			str, _ = strings.CutSuffix(s, "\"")
+
+			values = append(values, reflect.ValueOf(str))
+		}
+
+		return reflect.ValueOf(values), nil
+
 	default:
 		s, _ = strings.CutPrefix(s, "\"")
 		s, _ = strings.CutSuffix(s, "\"")
